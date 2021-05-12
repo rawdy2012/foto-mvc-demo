88f1ef5c6bbd5ab8c429c6f578da2ea3d5292bdf
AJAY SINGH YADAV <rawdy2012@gmail.com>
2021-05-12 12:32:26 +0530

    Add project files.

---
+++ MVC-Filters-Demo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Filters_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        
+        public ActionResult GetDate()
+        {
+            return View();
+        }
+        [OutputCache(Duration = 20)]
+        public string GetDay()
+        {
+            var result= DateTime.Now.DayOfWeek;
+            return result.ToString();
+        }
+    }
+}
---
+++ MVC-Filters-Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Filters_Demo.MvcApplication" Language="C#" %>
